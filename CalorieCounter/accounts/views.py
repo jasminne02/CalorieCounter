20ca04596ebdfa02f63657bdf9514514feb7c4ae
jasminne02 <jasmine.yabari02@gmail.com>
2022-12-06 21:58:52 +0200

    New commit

--- CalorieCounter/accounts/views.py
+++ CalorieCounter/accounts/views.py
@@ -7,39 +7,7 @@ from django.contrib.auth.models import Group
 from CalorieCounter.accounts.forms import CustomUserCreationForm, CustomUserLoginForm, CustomUserEditForm, \
     CustomUserDeleteForm
 from CalorieCounter.accounts.models import CustomUser
-
-
-def set_user_info(user):
-    set_user_calories_per_day(user)
-    set_user_fats_per_day(user)
-    set_user_protein_per_day(user)
-    set_user_carbs_per_day(user)
-    user.save(update_fields=('calories_per_day', 'fats_grams_per_day', 'proteins_grams_per_day', 'carbs_grams_per_day'))
-
-
-def set_user_calories_per_day(user):
-    """ Revised Harris-Benedict Equation """
-    if user.gender == 'Male':
-        """ 66.47 + (13.75 x weight in kg) + (5.003 x height in cm) - (6.755 x age in years) """
-        user.calories_per_day = 66.47 + (13.75 * user.weight) + (5.003 * user.height) - (6.755 * user.age)
-    elif user.gender == 'Female':
-        """ 655.1 + (9.563 x weight in kg) + (1.850 x height in cm) - (4.676 x age in years) """
-        user.calories_per_day = 655.1 + (9.563 * user.weight) + (1.850 * user.height) - (4.676 * user.age)
-
-
-def set_user_fats_per_day(user):
-    """ 30% of total calorie intake should come from fats. Each gram of fat is 'worth' 9 calories """
-    user.fats_grams_per_day = user.calories_per_day * 0.30 / 9
-
-
-def set_user_protein_per_day(user):
-    """ Around 20% of total calorie intake should come from proteins. Protein has four calories per gram """
-    user.proteins_grams_per_day = user.calories_per_day * 0.20 / 4
-
-
-def set_user_carbs_per_day(user):
-    """ Around 50% of total calorie intake should come from carbs. Carbohydrates have four calories per gram """
-    user.carbs_grams_per_day = user.calories_per_day * 0.5 / 4
+from CalorieCounter.accounts.functionality import *
 
 
 class UserRegisterView(views.CreateView):
--- CalorieCounter/core/views.py
+++ CalorieCounter/core/views.py
@@ -3,34 +3,11 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic as views
 
+from CalorieCounter.calorie_counter.functionality import get_daily_data
 from CalorieCounter.core.forms import MotivationsCreateForm
 from CalorieCounter.core.models import FoodPlan, Motivation
-from CalorieCounter.calorie_counter.views import get_daily_data, calculate_eaten_carbs, calculate_eaten_protein, \
-    calculate_eaten_fats, calculate_eaten_calories
 from CalorieCounter.accounts.models import CustomUser
-
-
-def is_user_in_group(request, value):
-    try:
-        return request.user.groups.get(name=value)
-    except Group.DoesNotExist:
-        return None
-
-
-def add_food_to_daily_data(daily_data, food):
-    for f in food:
-        daily_data.calories_eaten += calculate_eaten_calories(f, 100)
-        daily_data.fats_grams_per_day += calculate_eaten_fats(f, 100)
-        daily_data.carbs_grams_per_day += calculate_eaten_carbs(f, 100)
-        daily_data.proteins_grams_per_day += calculate_eaten_protein(f, 100)
-
-
-def add_meal_to_daily_data(daily_data, meals):
-    for meal in meals:
-        daily_data.calories_eaten += calculate_eaten_calories(meal, meal.grams)
-        daily_data.fats_grams_per_day += calculate_eaten_fats(meal, meal.grams)
-        daily_data.carbs_grams_per_day += calculate_eaten_carbs(meal, meal.grams)
-        daily_data.proteins_grams_per_day += calculate_eaten_protein(meal, meal.grams)
+from CalorieCounter.core.functionality import *
 
 
 def show_homepage(request):
