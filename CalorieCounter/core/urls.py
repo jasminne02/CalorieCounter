39b9c403dfc4541de78ee4e67570b6e6d6358b7f
jasminne02 <jasmine.yabari02@gmail.com>
2022-12-11 15:04:18 +0200

    New commit

--- CalorieCounter/core/urls.py
+++ CalorieCounter/core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.show_homepage, name='home'),
     path('about/', views.show_about_page, name='about'),
     path('food-plans/', views.FoodPlansView.as_view(), name='food plans'),
-    path('food-plan-details/<int:pk>', include([
+    path('food-plan-details/<int:pk>/', include([
         path('', views.FoodPlanDetailsView.as_view(), name='food plan details'),
         path('add-for-the-day/', views.add_plan_for_the_day, name='add plan for the day'),
     ])),
